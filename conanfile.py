068691a7b69d90c7b60d75321ba4f000ef38a4e6
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2023-03-06 11:29:14 +0300

    Added conanfile.py and CMakeLists.txt to the src folder.

---
+++ conanfile.py
@@ -0,0 +1,36 @@
+from conans import ConanFile, CMake, tools
+
+
+class DictionaryConan(ConanFile):
+    name = "DataStructures-201"
+    version = "1.0.0"
+    license = "GPL License"
+    author = "Olcay Taner Yildiz olcay.yildiz@ozyegin.edu.tr"
+    url = "https://github.com/olcaytaner/201-DataStructures-CPP"
+    description = "Data Structures library for 201 course"
+    topics = ("")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": False, "fPIC": True}
+    generators = "cmake"
+    exports_sources = "src/*", "Test/*"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="src")
+        cmake.build()
+
+    def package(self):
+        self.copy("*.h", src="src", dst="include")
+        self.copy("*DataStructures-201.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["DataStructures-201"]
